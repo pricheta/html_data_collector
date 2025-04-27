ae5b7305a27063f34ceea76c19452e01121ba612
pricheta <pricheta@inbox.ru>
2025-04-27 05:20:17 +0300

    Настроена возможность отправки get-запросов на сайт

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import pandas
+
 from httm_client.client import HttpClient
 
 
-if __name__ == "__main__":
-    google_response = HttpClient.send_get_request(url="https://google.com")
-    print(google_response)
+URL_PATTERN = "https://indy-towers.ru/search?korpus=4&section=1&floor={floor}&flat={flat}"
 
+
+if __name__ == "__main__":
+    df = pandas.read_excel("files/file.xlsx")
+    for _, row in df.iterrows():
+        floor = row[3]
+        flat = row[0]
+
+        url = URL_PATTERN.format(floor=floor, flat=flat)
+        response = HttpClient.send_get_request(url=url)
+
+        print(f"{floor=}, {flat=}")
